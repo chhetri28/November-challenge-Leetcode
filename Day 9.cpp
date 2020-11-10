745e76290571cac795feb5517656728d6a7fe160
chhetri28 <chettrisourav28@gmail.com>
2020-11-10 13:03:18 +0530

    Day 9

---
+++ Day 9.cpp
@@ -0,0 +1,9 @@
+//  Maximum Difference Between Node and Ancestor
+class Solution {
+public:
+    int maxAncestorDiff(TreeNode* r,int mn = 100000, int mx = 0) {
+        ;
+         return r ? max(maxAncestorDiff(r->left, min(mn, r->val), max(mx, r->val)),
+        maxAncestorDiff(r->right, min(mn, r->val), max(mx, r->val))) : mx - mn;
+    }
+};
