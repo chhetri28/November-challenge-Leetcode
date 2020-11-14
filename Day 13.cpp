32844c5eec485b64be428cdf63e0e431714a9731
chhetri28 <chettrisourav28@gmail.com>
2020-11-14 20:30:49 +0530

    Day 13

---
+++ Day 13.cpp
@@ -0,0 +1,19 @@
+//  Populating Next Right Pointers in Each Node
+class Solution {
+public:
+    Node* connect(Node* root) {
+        Node *pre = root, *cur;
+        while (pre) {
+            cur = pre;
+            while (cur && cur -> left) { 
+                cur -> left -> next = cur -> right;
+                if (cur -> next) {
+                    cur -> right -> next = cur -> next -> left;
+                }
+                cur = cur -> next;
+            }
+            pre = pre -> left;
+        }
+        return root;
+    }
+};
