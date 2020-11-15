dcabe2b1c9ac14c742bdfb66d59616161541ccad
chhetri28 <chettrisourav28@gmail.com>
2020-11-15 09:29:06 +0530

    Day 14

---
+++ Day 14.cpp
@@ -0,0 +1,7 @@
+// Poor Pigs
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+         return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
